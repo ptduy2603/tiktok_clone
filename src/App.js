087dd84a12eabe12442405bdb993dd98d6484e1e
Thanh Duy <duythanhpham2603@gmail.com>
2023-07-23 17:36:40 +0700

    add prettier

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Button from '~/components/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Button />
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <Button />
+        </div>
+    );
 }
 
 export default App;
