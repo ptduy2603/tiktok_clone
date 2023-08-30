e41ea73addee96b7abe4a90fcc198e3c6e76db1c
Thanh Duy <duythanhpham2603@gmail.com>
2023-08-30 15:46:33 +0700

    Update Sidebar

--- src/components/SuggestedAccount/SuggestedAccount.js
+++ src/components/SuggestedAccount/SuggestedAccount.js
@@ -16,14 +16,14 @@ function SuggestedAccount({ heading }) {
             <p className={cx('heading')}>{heading}</p>
             <ul className={cx('account-list')}>
                 <AccountItem
-                    username="Pham Thanh Duy"
-                    nickname="Béo"
+                    username="Phạm Thanh Duy"
+                    nickname="thanhdzi2610"
                     avatar="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/3e1404f4314db6355766c81ccda57c2b.jpeg?x-expires=1693450800&x-signature=5E8HHqrWdjwT6BCoeZRqfwk2rs8%3D"
                     check
                 />
                 <AccountItem
-                    username="Dương Ngọc Anh"
-                    nickname="Nah"
+                    username="Trần Tuyết Anh"
+                    nickname="anhtran2610"
                     avatar="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/751d9281c7f18830a694812b0643f720.jpeg?x-expires=1692111600&x-signature=Wjj5Ohh22xz3XEDq6%2F%2FuzMpDPKk%3D"
                 />
             </ul>
